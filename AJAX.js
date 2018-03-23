9542369a23b9b77e7dbbab00253d81ec81ddb42f
BY <878041280@qq.com>
2018-03-23 04:17:18 -0500

    modularization
    Add AJAX method to the object of window.

--- AJAX.js
+++ AJAX.js
@@ -7,64 +7,58 @@
         success:function(){},
         error:function(){}
     })
-
 */
 
-function AJAX(opt) {
-    opt = opt || {};
 
-    opt = {
-        url: opt.url || "",
-        type: (opt.type || 'GET').toUpperCase(),
-        async: opt.async ? opt.async : true,
-        data: opt.data || {},
-        success: opt.success || function(str) {},
-        error: opt.error || function() {}
-    }
+(function(window) {
+    
+    function AJAX(opt) {
+        opt = opt || {};
 
-    var xmlhttp;
-    if (window.XMLHttpRequest) {
-        xmlhttp = new XMLHttpRequest();
-    } else {
-        xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
-    }
-
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-            console.log('发送成功！');
-            opt.success(this.responseText);
-        } else if (this.readyState === 4 && this.status !== 200) {
-            console.log('发送失败！');
-            opt.error(this.status);
+        opt = {
+            url: opt.url || "",
+            type: (opt.type || 'GET').toUpperCase(),
+            async: opt.async ? opt.async : true,
+            data: opt.data || {},
+            success: opt.success || function(str) {},
+            error: opt.error || function(str) {}
         }
-    };
 
-    switch (opt.type) {
-        case "POST":
-            xmlhttp.open('POST', opt.url, opt.async);
-            xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            xmlhttp.send(format(opt.data));
-            break;
-        default:
-            xmlhttp.open('GET', opt.url + '?' + format(opt.data), opt.async);
-            xmlhttp.send();
-            break;
+        var format = function(obj) {
+            arr = new Array();
+            Object.keys(obj).forEach(function(item) {
+                arr.push(item + "=" + obj[item]);
+            });
+            return arr.join("&");
+        };
+
+        var xmlhttp;
+        if (window.XMLHttpRequest) {
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
+        }
+
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                opt.success(this.responseText);
+            } else if (this.readyState === 4 && this.status !== 200) {
+                opt.error(this.status);
+            }
+        };
+
+        switch (opt.type) {
+            case "POST":
+                xmlhttp.open('POST', opt.url, opt.async);
+                xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                xmlhttp.send(format(opt.data));
+                break;
+            default:
+                xmlhttp.open('GET', opt.url + '?' + format(opt.data), opt.async);
+                xmlhttp.send();
+                break;
+        }
+
     }
-}
-
-function format(obj) {
-    arr = new Array();
-    Object.keys(obj).forEach(function(item) {
-        arr.push(item + "=" + obj[item]);
-    });
-    return arr.join("&");
-}
-
-/*
-var testDemo = {
-    abc:123,
-    def:"pangwei",
-    ghi:'a'
-};
-
-console.log(format(testDemo));*/
+    window.AJAX = AJAX;
+})(window)
